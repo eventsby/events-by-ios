b21010ec7e36b643e92a9b1113e0b01b5d3e2cd9
Anton Polyakov <toxa95401@gmail.com>
2018-03-31 23:41:48 +0300

    Add EventsVC + storyboard, EventDetalsVC + storyboard

---
+++ EventsBy/Modules/Events/EventsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  EventsViewController.swift
+//  EventsBy
+//
+//  Created by Anton Polyakov on 3/31/18.
+//  Copyright © 2018 Events-By. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController {
+    
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
