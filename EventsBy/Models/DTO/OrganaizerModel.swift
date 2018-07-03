c700947660732932ff36b92937d26bc1e2e008a0
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2018-07-03 14:28:27 +0300

    Added Location, Organaizer, Participant DTO models

---
+++ EventsBy/Models/DTO/OrganaizerModel.swift
@@ -0,0 +1,9 @@
+//
+//  OrganaizerModel.swift
+//  EventsBy
+//
+//  Created by Alexander Tereshkov on 7/3/18.
+//  Copyright © 2018 Events-By. All rights reserved.
+//
+
+import Foundation
---
+++ EventsBy/Models/DTO/ParticipantModel.swift
@@ -0,0 +1,9 @@
+//
+//  ParticipantModel.swift
+//  EventsBy
+//
+//  Created by Alexander Tereshkov on 7/3/18.
+//  Copyright © 2018 Events-By. All rights reserved.
+//
+
+import Foundation
